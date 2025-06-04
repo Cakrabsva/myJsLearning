d86c3ef63ac31aeb9ce1752ff877ede6be40c9fa
Cakrabsva <cakrabilisairo.va@gmail.com>
2025-06-04 10:00:35 +0700

    Manual Slicer Challange - Done

--- challange/manualSlicer.js
+++ challange/manualSlicer.js
@@ -6,15 +6,36 @@
  * start = inclusive
  * end = exclusive
  * 
+ * example:
+ * ['ant', 'bison', 'camel', 'duck', 'elephant']
+ * console.log(manualSlicer(['ant', 'bison', 'camel', 'duck', 'elephant'], 2, 4)) // ['camel'.'duck']
+ * console.log(manualSlicer(['ant', 'bison', 'camel', 'duck', 'elephant'], 2)) // ['camel'.'duck', 'elephant']
+ * 
  */
 
 let dataSet = ['ant', 'bison', 'camel', 'duck', 'elephant']
 
 function manualSlicer (data, start, end) {
+    let dataExclude = [];
+    let dataInclude = [];
+    let messageBox = ""
 
-}
-
-console.log(manualSlicer(dataSet, 2, 4)) // ['camel'.'duck']
-
+    if(!data) {
+        messageBox = "invalid Data";
+        return messageBox;
+    } else {
+        for (let i = 0; i < data.length; i ++) {
+            if (i < start) {
+                dataExclude.push(data[i]);
+            } else if (i >= end) {
+                dataExclude.push(data[i]);
+            } else {
+                dataInclude.push(data[i]);
+            };
+        };
+    };
+    return dataInclude;
+};
+console.log(manualSlicer(['ant', 'bison', 'camel', 'duck', 'elephant'],2,4));
 
 
