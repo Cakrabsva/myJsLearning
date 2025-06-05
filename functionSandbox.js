3e9cb65cd1a4f9feefed33ed56cbf205ac44bb30
Cakrabsva <cakrabilisairo.va@gmail.com>
2025-06-05 10:23:59 +0700

    lerning modular function

--- functionSandbox.js
+++ functionSandbox.js
@@ -45,3 +45,60 @@ function aChecker (word) {
 for (let i = 0; i < str.length; i++) {
     //console.log(`Pada kata ${str[i]} terdapat ${aChecker(str[i])} huruf vokal a/A`)
 };
+
+/**
+ * Modular Function
+ * 
+ * => penggunaan fungsi kepada fungsi yang lain (fungsi didalam fungsi)
+ * => Main Function = fungsi yang menggunakan sub function
+ * => Sub Function  = fungsi yang mendukung fungsi utama / Main seperated function
+ * 
+ * 
+ */
+
+let plats = "2154,5568,2132,4424,117,6523"
+
+
+// SUB FUNCTION
+function formatedToArray (str) {
+    let formatedArray = [];
+    let stringContainer = ''
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === ",") {
+            formatedArray.push(Number(stringContainer));
+            stringContainer = '';
+        } else if (i === str.length-1) {
+            stringContainer += str[i];
+            formatedArray.push(Number(stringContainer));
+        } else {
+            stringContainer += str[i];
+        };
+    };
+    return formatedArray;
+};
+
+// SUB FUNCTION
+function reporting (evenPlat, oddPlat) {
+     return outputMessage = `We have ${evenPlat} even plats and ${oddPlat} oddPlat`;
+};
+
+// MAIN FUNCTION
+function findEvenOdd (str) {
+    let platNumbers = formatedToArray(str)
+    let oddPlat = 0;
+    let evenPlat = 0;
+    let reportStatement = ''
+
+    for (let i = 0; i < platNumbers.length; i++) {
+        if (platNumbers[i] % 2 === 0) {
+            evenPlat++;
+        } else {
+            oddPlat++;
+        };
+    };
+    reportStatement = reporting(evenPlat, oddPlat);
+    return reportStatement;
+};
+
+console.log(findEvenOdd(plats));
