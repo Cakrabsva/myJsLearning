3703545b034b3facab8714d89f6f08b3ba67f2b9
Cakrabsva <cakrabilisairo.va@gmail.com>
2025-06-11 20:45:23 +0700

    adding some notes to looping sandbox

--- loopingSandbox.js
+++ loopingSandbox.js
@@ -5,6 +5,7 @@
  */
 
 //for digunakan untuk pengulangan dimana sudah diketahui jumlah pengulanganya;
+let myNumber = [1,2,3,4,5,6]
 for (let i = 0; i < myNumber.length; i++) {
     if (myNumber[i] % 2 === 0) {
         //console.log(`${myNumber[i]} adalah angka genap`);
@@ -36,3 +37,22 @@ do {
     //console.log("ini dari Do...While")
     j++
 } while (j < 3);
+
+
+//================ Looping Forms =================
+
+const polaris = [1,2,3,4,5,6]
+
+// form 1 - Regular for looping
+for (let i = 0; i < polaris.length; i ++) {
+    let data = polaris[i]
+    console.log(data)
+}
+
+//form 2 - for-of Looping
+for (let data of polaris) {
+    console.log(data)
+}
+
+//form 3 - map looping
+let data = polaris.map((el) => {console.log(el)})
